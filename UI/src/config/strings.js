8f50d0473c75078b6afb11f848c61966a0c9151b
jatinsachdev12 <103931804+jatinsachdev12@users.noreply.github.com>
2023-01-06 18:51:07 +0530

    HD--169

--- UI/src/config/strings.js
+++ UI/src/config/strings.js
@@ -4,6 +4,7 @@ export const Strings = {
     GLEREN_CONTACT_US_PATH: "/contact",
     CREATED_SUCCESSFULLY: "Created Successfully",
     UPDATED_SUCCESSFULLY: "Updated Successfully",
+    PROFILE_IMAGE_UPDATED_SUCCESSFULLY:"Profile Image Updated Successfully",
     PROJECT_NAME_MANDATORY: "Project Name is Mandatory",
     DEPARTMENT_NAME_MANDATORY: "Department Name is Mandatory",
     STATUS_NAME_MANDATORY: "STATUS Name is Mandatory",
